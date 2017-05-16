3e36a77eb2fa91280aa47bfa01b4eccc9db8c3c7
Daniil Levin <dzlevin@edu.hse.ru>
2017-05-16 19:49:28 +0300

    Added page navigation, authorization and images

---
+++ DoorBoutique/Pages/DoorAssortment.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoorBoutique
+{
+    /// <summary>
+    /// Логика взаимодействия для DoorAssortment.xaml
+    /// </summary>
+    public partial class DoorAssortment : Page
+    {
+        public DoorAssortment()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.MainPage);
+        }
+
+        private void DoorPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.DoorPage);
+        }
+
+        private void ShopPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.ShopPage);
+        }
+
+        private void loginToolboxButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.LogIn);
+        }
+    }
+}
---
+++ DoorBoutique/Pages/DoorPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoorBoutique
+{
+    /// <summary>
+    /// Логика взаимодействия для DoorPage.xaml
+    /// </summary>
+    public partial class DoorPage : Page
+    {
+        public DoorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.MainPage);
+        }
+
+        private void DoorPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.DoorPage);
+        }
+
+        private void ShopPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.ShopPage);
+        }       
+
+        private void loginToolboxButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.LogIn);
+        }
+
+        private void OpenCatalog_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(Pages.DoorAssortment);
+        }
+    }
+}
